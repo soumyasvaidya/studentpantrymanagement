28cd05ef5a5e36400db7e6764238a72987651330
Arati Khanolkar <aratik@umich.edu>
2023-12-01 22:04:26 -0500

    adding code for get product by id

--- studentPantry/src/main/java/com/student/pantry/studentPantry/controller/ProductController.java
+++ studentPantry/src/main/java/com/student/pantry/studentPantry/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,18 @@ public class ProductController {
     @GetMapping
     public List<Products> getAllProducts() {
         return productService.getAllProducts();
+    }
+    
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/{productId}")
+    public ResponseEntity<Products> getProductById(@PathVariable Long productId) {
+        Products product = productService.getProductById(productId);
+
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
